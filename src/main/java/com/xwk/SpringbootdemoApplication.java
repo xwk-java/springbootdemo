0b447470600863bda73b5ceb53d639533e88c0f1
xwk <1065428425@qq.com>
2019-12-20 21:09:01 +0800

    33

--- src/main/java/com/xwk/SpringbootdemoApplication.java
+++ src/main/java/com/xwk/SpringbootdemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
     }
